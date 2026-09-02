609d15bfb0d4dba6fef8c4639e5c3258d0c29fc5
MisterMallard <calvie.bo.balvie@gmail.com>
2026-09-02 13:00:57 +0200

    test

---
+++ Assets/Calvin/Indicator.cs
@@ -0,0 +1,20 @@
+using UnityEditor.Search;
+using UnityEngine;
+public class Indicator : MonoBehaviour
+{
+	bool active = false;
+	// Start is called once before the first execution of Update after the MonoBehaviour is created
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (active){
+        
+        }
+
+    }
+}
