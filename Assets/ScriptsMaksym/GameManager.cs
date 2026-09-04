d546e854a26bea43f1b1bf3d5c0bdc0c0acd3cff
Names <40932@ma-web.nl>
2026-09-04 12:27:37 +0200

    merge2

--- Assets/ScriptsMaksym/GameManager.cs
+++ Assets/ScriptsMaksym/GameManager.cs
@@ -1,5 +1,5 @@
 using System.Collections;
-using UnityEditor.ShaderGraph.Internal;
+
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
--- Assets/scripts/readDatafromSerialPort.cs
+++ Assets/scripts/readDatafromSerialPort.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections;
 using System.IO.Ports;
 using System.Web;
-using Unity.VisualScripting;
+
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using Microsoft.Win32;
